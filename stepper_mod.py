1fa7bc4f60a97b388ea388ec4c3a11917c869c1c
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-11-02 03:09:24 +0000

    debugging

--- stepper_mod.py
+++ stepper_mod.py
@@ -25,7 +25,7 @@ def delay_us(tus): # use microseconds to improve time resolution
 def halfstep(dir):
   #dir=+/- 1 (ccw/cw)
   print("in the halfstep(dir) function")
-  state += dir #increment forward, decrement reverse
+  state = dir + 1#increment forward, decrement reverse
   print("state= "+str(state))
   #we dont want to go past the list. if we rolloff reset ourselves at beginning open
   if state>7: state=0 # we really ony need to check 8 or -1
